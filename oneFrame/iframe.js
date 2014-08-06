a44a5666af6f26b32fecbd5ce5c30e51d85579d9
Sukhjinderjeet Singh <sabhi7@hotmail.com>
2014-08-05 21:49:26 -0400

    changed js for onlu one frame per bill

--- oneFrame/iframe.js
+++ oneFrame/iframe.js
@@ -1,21 +1,29 @@
 (function(){
-	var width,height;
+	var width,height,iactive;
 	
 	/* initalization */
 	addEvent('DOMContentLoaded',document,function(){
-		
 		vtcparent=document.getElementsByClassName('vtc-bills');
 		vtciframes=document.getElementsByClassName('vtc-iframe');
-		
 		for(var i=0;i<vtcparent.length;i++){
 			addEvent('mouseover',vtcparent[i],display);
 			addEvent('click',vtcparent[i],display);
 		}	
 	});
 	
-	 addEvent('click', document, function() {
-     hideFrames();
-    });
+  addEvent('click', document, function() {
+    hideFrames();
+    setActive(undefined);
+  });
+
+  addEvent('touchstart', document, function() {
+    hideFrames();
+    setActive(undefined);
+  });
+
+  addEvent('resize', window, function() {
+    if (typeof iactive !== 'undefined') display.apply(iactive);
+  });
 
 	function addEvent(event, el, fn){
 		if (el.addEventListener){
@@ -43,28 +51,34 @@
   }
   /*display iframe next to the active bill*/	
   function display(){
-  	hideFrames();
+    hideFrames();
+    removeClass(this, 'vtc\\-i\\-.*');
   	var rel=this.getAttribute('data-vtctarget');
   	for(var i=0;i<vtciframes.length;i++){
   		if(vtciframes[i].getAttribute('data-vtcparent') == rel){
-  			addClass(vtciframes[i],myposition(this,vtciframes[i]));
+  			addClass(this,myposition(this,vtciframes[i]));
   			removeClass(vtciframes[i],'vtc-iframeH');
   		}
   	}
+    setActive(this);
   }	
 
   function myposition(ele,iframe){
   	var p = ele.getBoundingClientRect();
     width = document.documentElement.clientWidth;
     height = document.documentElement.clientHeight;
-    var left=ele.offsetLeft;
+    var left=getOffsetLeft(ele);
+    var right=left+ele.offsetWidth;
+    var top=getOffsetTop(ele);
+    var bottom=top + ele.offsetHeight;
+   /* var left=ele.offsetLeft;
     var right=ele.offsetLeft + ele.offsetWidth;
     var top=ele.offsetTop;
-    var bottom=ele.offsetTop + ele.offsetHeight;
+    var bottom=ele.offsetTop + ele.offsetHeight;*/
     resetFrame(iframe);
     /*dispaly on right*/
     if ((width-p.right) > 510) {
-    	iframe.style.left=right+25 +"px";	
+    	iframe.style.left=right+20 +"px";	
       if (topBottom(p.top,p.bottom)) {
       	iframe.style.bottom=height-bottom-70 +"px";/*towards the top*/
       } else{
@@ -74,7 +88,7 @@
 	  }
 	  /*display on the left*/
     if (p.left > 510) {
-    	iframe.style.right=width-left+25 +"px";
+    	iframe.style.right=width-left+20 +"px";
       if(topBottom(p.top,p.bottom)) {
       	iframe.style.bottom=height-bottom-70 +"px";
       } else{
@@ -84,7 +98,7 @@
  		}
  		/*display on the bottom*/
     if (p.left < (width/2)) {
-    	iframe.style.top=bottom+25 +"px";	
+    	iframe.style.top=bottom+23 +"px";	
 	    if (p.left < (width/4)){
 	    	iframe.style.left=left-30 +"px";
 	    } else{
@@ -92,7 +106,7 @@
 	    }
 	    return "vtc-i-bottom"; 
     } else {
-	    iframe.style.top=bottom+25 +"px";
+	    iframe.style.top=bottom+23 +"px";
 	    if ((width-p.right) < (width/4)){
 	    	iframe.style.right=width-right-30 +"px";
    		} else{
@@ -118,14 +132,34 @@
       addClass(vtciframes[i], 'vtc-iframeH');
     }
   }
+
+  function setActive(ele) {
+    if (typeof iactive !== 'undefined') removeClass(iactive, 'vtc-active');
+    if (typeof ele !== 'undefined') addClass(ele, 'vtc-active');
+    iactive = ele;
+  }
+
+  function getOffsetLeft( elem )
+  {
+    var offsetLeft = 0;
+    do {
+      if ( !isNaN( elem.offsetLeft ) ) 
+        offsetLeft += elem.offsetLeft;
+    } while( elem = elem.offsetParent );
+    return offsetLeft;
+  }
+
+  function getOffsetTop( elem )
+  {
+    var offsetTop = 0;
+    do {
+      if ( !isNaN( elem.offsetTop ) )
+        offsetTop += elem.offsetTop;
+    } while( elem = elem.offsetParent );
+    return offsetTop;
+  }
+
 })();
-
-
-
-
-
-
-
 
 
 /*function getAllElementsWithAttribute(attribute)
@@ -142,16 +176,4 @@
   }
   return matchingElements;
 }
-function getOffsetLeft( elem )
-{
-    var offsetLeft = 0;
-    do {
-      if ( !isNaN( elem.offsetLeft ) )
-      {
-          offsetLeft += elem.offsetLeft;
-      }
-    } while( elem = elem.offsetParent );
-    return offsetLeft;
-}
-
 */
